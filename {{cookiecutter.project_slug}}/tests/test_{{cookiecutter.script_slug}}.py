470a82467dc9a277dfdd8709b2add7d1794d32c8
Ben Fulroth <fulrothb@icloud.com>
2020-06-08 16:40:57 -0400

    Changes to rendered projects testmodule

--- {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.script_slug}}.py
+++ {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.script_slug}}.py
@@ -1,10 +1,27 @@
+"""Module for testing {{cookiecutter.script_slug}}"""
 
 from unittest import TestCase
+from unittest.mock import patch
+import pandas as pd
+from click.testing import CliRunner
+import numpy as np
 
 from {{cookiecutter.script_slug.cli}} import main
 
 
 class test_Cli(TestCase):
+
+    def test_Cli_somthing(self):
+
+        # Use the click CliRunner object for testing Click implemented Cli programs.
+        runner = CliRunner()
+        result = runner.invoke(main, ['--option_1', 'EXAMPLE_path_to_fixture_', 'option_2',
+                                                        'input_2'])
+
+        self.assertEqual(0, result.exit_code)
+
+
+class test_somthing(TestCase):
 
     @classmethod
     def setUpClass(cls) -> None:
@@ -16,5 +33,5 @@ class test_Cli(TestCase):
         cls.var_name = ''
         pass
 
-    def test_1(self):
+    def test_somthing(self):
         pass

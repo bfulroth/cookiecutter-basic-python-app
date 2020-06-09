41672e67408cd34fc6d793e30a896879e0ba2252
Ben Fulroth <fulrothb@icloud.com>
2020-06-09 08:12:35 -0400

    Fixed one out of 2 failed pytest-cookie tests

--- tests_template/test_bake_project.py
+++ tests_template/test_bake_project.py
@@ -1,8 +1,8 @@
-from contextlib import contextmanager
+"""Module for testing the rendering of the template script"""
 
+from contextlib import contextmanager
 import os
 import subprocess
-
 
 @contextmanager
 def inside_dir(dirpath):
